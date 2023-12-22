e54c879af5384b7ea0f863cbbe0d954af6ea3ef6
HarveyXocop <stuardouzumaki@hotmail.com>
2023-12-22 02:09:49 -0600

    models y superuser

--- academia/academia/core/erp/models.py
+++ academia/academia/core/erp/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 # Create your models here.
+
+class Type(models.Model):
+    name = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.names
+    
+    class Meta:
+        verbose_name ='Tipo'
+        verbose_name_plural ='Tipos'
+        db_table = 'Tipos'
+        ordering = ['id']
+
+
 class Usuario(models.Model):
     names = models.CharField(max_length=150)#,verbose_name='Nombres')
     appellidos = models.CharField(max_length=150)#,verbose_name='Apellidos')
@@ -8,6 +22,7 @@ class Usuario(models.Model):
     name_usuario = models.CharField(max_length=150)#,unique=True,verbose_name='Nombre de Usuario')
     correo = models.CharField(max_length=150)#,unique=True,verbose_name='Correo Electronico')
     password = models.CharField(max_length=150)#,verbose_name='Password')
+    type = models.ForeignKey(Type,on_delete=models.CASCADE)   #Relacionando tabla
     #flotante = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
     def __str__(self):
         return self.names
@@ -18,4 +33,38 @@ class Usuario(models.Model):
         db_table = 'Usuarios'
         ordering = ['id']
 
+
+class Cursos(models.Model):
+    curso = models.CharField(max_length=150)
+    descripcion = models.CharField(max_length=150)
+    imagen = models.ImageField(upload_to='cursos/%Y/%m/%d', null=True, blank=True)
+    costo = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+
+    def __str__(self):
+        return self.curso
     
+    class Meta:
+        verbose_name ='Curso'
+        verbose_name_plural ='Cursos'
+        db_table = 'Cursos'
+        ordering = ['id']
+
+
+class Asignacion(models.Model):
+    curso = models.ForeignKey(Cursos,on_delete=models.CASCADE)
+    precio = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    cantidad = models.PositiveIntegerField(default=0)
+    nit =  models.PositiveIntegerField(default=0)
+    direccion = models.CharField(max_length=150)
+    forma_pago = models.CharField(max_length=100)
+    nombre_alumno = models.ForeignKey(Usuario,on_delete=models.CASCADE) 
+    correo_conf = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.curso
+    
+    class Meta:
+        verbose_name ='Asignacion'
+        verbose_name_plural ='Asignaciones'
+        db_table = 'Asignacion'
+        ordering = ['id']        
